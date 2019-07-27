1f7b310fa5fa581302e1b148df33bece5b05285f
giovanni-siddhartha <giovannisiddhartha252@gmail.com>
2019-07-27 19:15:40 +0300

    k armed bandit script

---
+++ k_armed_bandit.py
@@ -0,0 +1,54 @@
+# implements the 10-armed testbed in 2.3 of the book named "Reinforcement Learning An Introduction" by Richard S. Sutton
+# and Andrew G. Barto
+import numpy as np
+import matplotlib.pyplot as plt
+
+k = 10  # number of arms
+m = 2000  # number of different k-bandit problems
+the_mean = 0.0  # the mean of the normal distribution for the true value of the action value function q(t)
+the_variance = 1.0  # the variance of the normal distribution for the true value of the action value function q(t)
+num_of_timesteps = 2000  # number of time steps a bandit problem is run
+epsilon_array = [0.0, 0.01, 0.1]
+color_array = ["black", "red", "blue"]
+epsilon = 0.1  # the probability for epsilon greedy action
+t = range(num_of_timesteps)
+
+for epsilon, the_color in zip(epsilon_array, color_array):
+    # each row has the true values of the k action value functions, there are m rows each of which corresponds to an
+    # independent bandit problem
+    testbed_matrix = np.random.normal(the_mean, the_variance, (m, k))
+
+    reward_time_matrix = np.zeros((m, num_of_timesteps))
+
+    for problem_index in range(m):
+        mean_value = testbed_matrix[problem_index]
+        total_reward = np.zeros(k)
+        action_occurrence = np.zeros(k)
+        estimated_q = np.zeros(k)
+        reward_time_variation = np.zeros(num_of_timesteps)
+        for time_step in range(num_of_timesteps):
+            greedy = np.random.binomial(1, (1 - epsilon))
+            if greedy == 1:
+                action_index = np.argmax(estimated_q)
+            else:
+                action_index = np.random.choice(k)
+
+            reward = np.random.normal(mean_value[action_index], 1.0)
+            total_reward[action_index] = total_reward[action_index] + reward
+            action_occurrence[action_index] = action_occurrence[action_index] + 1
+            estimated_q[action_index] = total_reward[action_index] / action_occurrence[action_index]
+            reward_time_variation[time_step] = reward
+        reward_time_matrix[problem_index] = reward_time_variation
+
+    average_reward_time = np.mean(reward_time_matrix, axis=0)
+
+    plt.plot(t, average_reward_time, the_color, label=repr(epsilon))
+    plt.legend()
+
+
+plt.xlabel("time")
+plt.ylabel("average reward")
+plt.title("epsilon greedy with varying epsilon")
+plt.grid()
+plt.savefig("test.png")
+plt.show()
